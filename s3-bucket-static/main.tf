804c8a6a5894a10c43dad0a926135bf2b94e7551
caiiohenryk <caiohc.dev@gmail.com>
2026-08-01 12:15:19 -0300

    fix: resolve Terraform validation

--- s3-bucket-static/main.tf
+++ s3-bucket-static/main.tf
@@ -1,43 +1,48 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 variable "bucket_name" {
-    type = string
+  type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
-    bucket = "${var.bucket_name}"
+  bucket = var.bucket_name
 
-    website = {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 
-    tags = {
-        Name = "Website Static Bucket"
-        Environment = "QA"
-    }
+  tags = {
+    Name        = "Website Static Bucket"
+    Environment = "QA"
+  }
 }
 
-resource "aws_s3_public_access_block" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
+resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
+  bucket = aws_s3_bucket.static_site_bucket.id
 
-    block_public_acls = false
-    block_public_policy = false
-    ignore_public_acls = false
-    restrict_public_buckets = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.id
 
-    rule = {
-        object_ownership = "BucketOwnerPreferred"
-    }
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
-    acl = "public-read"
+  depends_on = [
+    aws_s3_bucket_ownership_controls.static_site_bucket,
+    aws_s3_bucket_public_access_block.static_site_bucket,
+  ]
+
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
 }
